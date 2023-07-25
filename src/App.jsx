2d227df0808649ba348233b1ebf2b11ba765b43e
IvanArjonaAmadorUPM <ivan.arjona.amador@alumnos.upm.es>
2023-07-25 19:07:09 +0200

    #1 feat: add basic porfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter } from "react-router-dom"
+import  {About, Contact, Experience, Feedbacks, 
+  Hero, Navbar, Works, StarsCanvas, Tech } from './components'
 const App = () => {
 
   return (
-    <div> Iván 3d Porfolio</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-o">
+        <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
